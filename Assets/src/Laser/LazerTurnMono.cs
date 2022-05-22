fd40ac2c060f1c7ee757ffb212c90159b45b95da
erkan <etaylan13@gmail.com>
2022-05-23 00:00:27 +0300

    init

---
+++ Assets/src/Laser/LazerTurnMono.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace src.Laser
+{
+    public class LazerTurnMono : MonoBehaviour
+    {
+        public float rotationSpeed = 1f;
+        
+        private void Update()
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+    }
+}
